7f482931f70ab3cfbd3be1e515e6c02a13171458
KarthikUdayMurthy <u.karthik99@gmail.com>
2020-10-16 18:05:02 +0530

    Download option initial commit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  AfterViewInit
+} from "@angular/core";
 import { ICard } from "../shared/card";
 import { CardService } from "../shared/card.service";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: "card",
   templateUrl: "./card.component.html",
   styleUrls: ["./card.component.css"]
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, AfterViewInit {
   constructor(public cardServ: CardService) {}
 
   showBack: boolean = false;
@@ -17,20 +24,28 @@ export class CardComponent implements OnInit {
 
   cardData: ICard;
 
+  @ViewChild("cardFront", { static: false }) cardFront: ElementRef;
+  @ViewChild("downloadLink", { static: false }) downloadLink: ElementRef;
+
   ngOnInit() {
     this.cardData = this.cardServ.cards[this.cardServ.selectedCardIndex];
     this.cardServ.sortExp(this.cardData.experience);
     this.cardServ.onEditGoToSlide = 0;
     this.cardServ.onEditGoToOrg = -1;
-    if(this.cardServ.cardViewStatus.side === "front") {
+    if (this.cardServ.cardViewStatus.side === "front") {
       this.showFront();
-    } else if(this.cardServ.cardViewStatus.side === "back") {
+    } else if (this.cardServ.cardViewStatus.side === "back") {
       this.showBack = false;
       setTimeout(() => {
-        this.showDetail(this.cardServ.cardViewStatus.detailType, this.cardServ.cardViewStatus.detailOrg);
+        this.showDetail(
+          this.cardServ.cardViewStatus.detailType,
+          this.cardServ.cardViewStatus.detailOrg
+        );
       }, 300);
     }
   }
+
+  ngAfterViewInit() {}
 
   getOrgs(): Array<any> {
     var expArray = this.cardData.experience;
@@ -116,15 +131,34 @@ export class CardComponent implements OnInit {
       });
     }
     this.showBack = true;
-    this.cardServ.cardViewStatus =  {side: "back", detailType: typ, detailOrg: org};
+    this.cardServ.cardViewStatus = {
+      side: "back",
+      detailType: typ,
+      detailOrg: org
+    };
   }
 
   showFront(): void {
-    this.cardServ.cardViewStatus =  {side: "front", detailType: "", detailOrg: ""};
+    this.cardServ.cardViewStatus = {
+      side: "front",
+      detailType: "",
+      detailOrg: ""
+    };
     this.cardServ.onEditGoToSlide = 0;
     this.showBack = false;
     this.cardbackHdr = "";
     this.cardDetailShow = "";
     this.expDetail = [];
+  }
+
+  downloadCard(): void {
+    html2canvas(this.cardFront.nativeElement).then(canvas => {
+      let cardLink = this.downloadLink.nativeElement;
+      cardLink.href = canvas
+        .toDataURL("image/png")
+        .replace("image/jpeg", "image/octet-stream");
+      cardLink.download = this.cardData.fullName + ".png";
+      cardLink.click();
+    });
   }
 }
